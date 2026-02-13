b976f7980cba2b99fab35246893ae27a5f71d409
Vishant <44126013+vishantbhole@users.noreply.github.com>
2026-02-13 06:11:30 -0600

    Add set method to TimeMap class

--- TimeMap.py
+++ TimeMap.py
@@ -3,3 +3,7 @@
 class TimeMap:
     def __init__(self):
         self.map = {}
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.map:
+            self.map[key] = []
+        self.map[key].append([value, timestamp])
