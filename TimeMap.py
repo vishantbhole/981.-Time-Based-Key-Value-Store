239e7f7243fe671a9101b0295b52b6ec1c2272ed
Vishant <44126013+vishantbhole@users.noreply.github.com>
2026-02-14 23:01:39 -0600

    Update TimeMap.py

--- TimeMap.py
+++ TimeMap.py
@@ -30,3 +30,6 @@ if __name__ == "__main__":
         if op == "TimeMap":
             obj = TimeMap()
             out.append(None)
+        elif op == "set":
+            obj.set(a[0], a[1], a[2])
+            out.append(None)
